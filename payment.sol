dfeacf38b65f707d750f848003018d2c0fc525cb
Levertz <95511599+lennardevertz@users.noreply.github.com>
2022-06-01 19:01:14 -0400

    Update payment.sol

--- payment.sol
+++ payment.sol
@@ -1,28 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.1; 
-
-interface ERC20 {
-    function balanceOf(address _tokenOwner) external view returns (uint balance);
-    function transfer(address _to, uint _tokens) external returns (bool success);
-    function approve(address _spender, uint256 _value) external returns (bool success);
-    function allowance(address _contract, address _spender) external view returns (uint256 remaining);
-    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
-}
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract payments {
+
+    using SafeMath for uint256;
+    mapping(string => uint256) percent;
     mapping(address => bool) private admins;
     address public contractOwner = msg.sender; 
-    mapping(string => address) public paymentToken;
-    mapping(uint => address) public receipts;
-    mapping(uint => uint) public amounts;
-    mapping(uint => string) public paymentType;
+    mapping(bytes32 => address) public receipts;
+    mapping(bytes32 => uint256) public amounts;
+    mapping(address => uint256) public balanceOf; 
+    mapping(string => address) public delegate;
 
+    constructor() {
+        delegate["IDriss"] = contractOwner;
+        percent["IDriss"] = 100;
+    }
 
-    event PaymentDone(address payer, uint amount, string token, uint paymentId, uint date);
+    event PaymentDone(address payer, uint256 amount, bytes32 paymentId_hash, string IDrissHash, uint256 date);
     event AdminAdded(address indexed admin);
     event AdminDeleted(address indexed admin);
-    event TokenAdded(string tokenHandle, address indexed tokenAddress);
-    event TokenDeleted(string tokenHandle, address indexed tokenAddress);
+    event DelegateAdded(string delegateHandle, address indexed delegateAddress);
+    event DelegateDeleted(string delegateHandle, address indexed delegateAddress);
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     function addAdmin(address adminAddress) external {
@@ -37,61 +37,86 @@ contract payments {
         emit AdminDeleted(adminAddress);
     }
 
-    function addToken(address tokenAddress, string memory tokenHandle) external {
-        require(msg.sender == contractOwner, "Only contractOwner can add payment token.");
-        paymentToken[tokenHandle] = tokenAddress;
-        emit TokenAdded(tokenHandle, tokenAddress);
+    function addDelegateException(address delegateAddress, string memory delegateHandle, uint256 percentage) external {
+        require(msg.sender == contractOwner, "Only contractOwner can add special delegate partner.");
+        require(delegate[delegateHandle] == address(0), "Delegate handle exists.");
+        require(delegateAddress != address(0), "Ownable: delegateAddress is the zero address.");
+        delegate[delegateHandle] = delegateAddress;
+        percent[delegateHandle] = percentage;
+        emit DelegateAdded(delegateHandle, delegateAddress);
     }
 
-    function deleteToken(string memory tokenHandle) external {
-        require(msg.sender == contractOwner, "Only contractOwner can delete payment token.");
-        address deletedToken = paymentToken[tokenHandle];
-        delete paymentToken[tokenHandle];
-        emit TokenDeleted(tokenHandle, deletedToken);
+    // Anyone can create a delegate link for anyone
+    function addDelegate(address delegateAddress, string memory delegateHandle) external {
+        require(delegate[delegateHandle] == address(0), "Delegate handle exists.");
+        require(delegateAddress != address(0), "Ownable: delegateAddress is the zero address.");
+        delegate[delegateHandle] = delegateAddress;
+        percent[delegateHandle] = 20;
+        emit DelegateAdded(delegateHandle, delegateAddress);
     }
 
-    function payToken(uint amount, uint paymentId, string memory token) external {
-        require(paymentToken[token] != address(0), "Token not accepted as payment.");
-        require(receipts[paymentId] == address(0), "Already paid this receipt.");
-        ERC20 paymentTc = ERC20(paymentToken[token]);
-        require(paymentTc.allowance(msg.sender, address(this)) >= amount, "Insuficient Allowance.");
-        require(paymentTc.transferFrom(msg.sender, address(this), amount), "Transfer Failed.");
-        receipts[paymentId] = msg.sender;
-        amounts[paymentId] = amount;
-        paymentType[paymentId] = token;
-        emit PaymentDone(receipts[paymentId], amounts[paymentId], paymentType[paymentId], paymentId, block.timestamp);
+    // Delete the delegation link if needed.
+    function deleteDelegate(string memory delegateHandle) external {
+        require(msg.sender == delegate[delegateHandle], "Only delegate can delete delegation link.");
+        address deletedDelegate = delegate[delegateHandle];
+        delete delegate[delegateHandle];
+        delete percent[delegateHandle];
+        emit DelegateDeleted(delegateHandle, deletedDelegate);
     }
 
-    function withdrawTokens(address tokenContract) external payable{
-        require(admins[msg.sender] == true, "Only trusted admin can withdraw.");
-        ERC20 tc = ERC20(tokenContract);
-        tc.transfer(msg.sender, tc.balanceOf(address(this)));
+    // Payment function distributing the payment into two balances.
+    function payNative(bytes32 paymentId_hash, string memory IDrissHash, string memory delegateHandle) external payable {
+        require(receipts[paymentId_hash] == address(0), "Already paid this receipt.");
+        receipts[paymentId_hash] = msg.sender;
+        amounts[paymentId_hash] = msg.value;
+        if (delegate[delegateHandle] != address(0)) {
+            balanceOf[contractOwner] += msg.value.sub((msg.value.mul(percent[delegateHandle])).div(100));
+            balanceOf[delegate[delegateHandle]] += (msg.value.mul(percent[delegateHandle])).div(100);
+        } else {
+            balanceOf[contractOwner] += msg.value;
+        }
+        emit PaymentDone(receipts[paymentId_hash], amounts[paymentId_hash], paymentId_hash, IDrissHash, block.timestamp);
     }
 
-    function payNative(uint paymentId) external  payable{
-        require(receipts[paymentId] == address(0), "Already paid this receipt.");
-        receipts[paymentId] = msg.sender;
-        amounts[paymentId] = msg.value;
-        paymentType[paymentId] = "ETH";
-        emit PaymentDone(receipts[paymentId], amounts[paymentId], paymentType[paymentId], paymentId, block.timestamp);
-    }
-
-     function withdraw() external returns (bytes memory) {
-        require(admins[msg.sender] == true, "Only trusted admin can withdraw.");
-        (bool sent, bytes memory data) = msg.sender.call{value: address(this).balance, gas: 40000}("");
+    // Anyone can withraw funds to any participating delegate
+    function withdraw(uint256 amount, string memory delegateHandle) external returns (bytes memory) {
+        require(amount <= balanceOf[delegate[delegateHandle]]);
+        balanceOf[delegate[delegateHandle]] -= amount;
+        (bool sent, bytes memory data) = delegate[delegateHandle].call{value: amount, gas: 40000}("");
         require(sent, "Failed to  withdraw");
         return data;
     }
 
+    // commit payment hash creation
+    function hashReceipt(string memory receiptId, address paymAddr) public pure returns (bytes32) {
+        return keccak256(abi.encode(receiptId, paymAddr));
+    }
+
+    // reveal payment hash
+    function verifyReceipt(string memory receiptId, address paymAddr) public view returns (bool) {
+        require(receipts[hashReceipt(receiptId, paymAddr)] == paymAddr);
+        return true;
+    }
+
+    // Transfer contract ownership
     function transferContractOwnership(address newOwner) public payable {
         require(msg.sender == contractOwner, "Only contractOwner can change ownership of contract.");
         require(newOwner != address(0), "Ownable: new contractOwner is the zero address.");
         _transferOwnership(newOwner);
     }
 
+    // Helper function
     function _transferOwnership(address newOwner) internal virtual {
         address oldOwner = contractOwner;
+        // transfer balance of old owner to new owner
+        uint256 ownerAmount = balanceOf[oldOwner];
+        // delete balance of old owner
+        balanceOf[oldOwner] = 0;
         contractOwner = newOwner;
+        // set new owner
+        delegate["IDriss"] = newOwner;
+        // set balance of new owner
+        balanceOf[newOwner] = ownerAmount;
         emit OwnershipTransferred(oldOwner, newOwner);
     }
 }
