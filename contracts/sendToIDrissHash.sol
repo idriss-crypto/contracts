0a18980ce50e4eb198c2a1f84be78ef80b4507d4
Rafal Kalinowski <kalinowskirafal.fm@gmail.com>
2022-06-14 08:10:13 +0200

    improvements

--- contracts/sendToIDrissHash.sol
+++ contracts/sendToIDrissHash.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.14;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "hardhat/console.sol";
 
 interface IDriss {
     function getIDriss(string memory hashPub)
@@ -29,18 +30,20 @@ enum AssetType {
     NFT
 }
 
+//TODO: remove console.log after testing
 /**
  * @title sendToHash
  * @author Rafał Kalinowski
  * @notice This contract is used to pay to the IDriss address without a need for it to be registered
  */
 contract sendToHash is Ownable {
-    // payer => beneficiary => assetAddress => AssetLiability
-    mapping(address => mapping(address => mapping(address => AssetLiability))) payerAssetMap;
-    // beneficiary => assetAddress => AssetLiability
-    mapping(address => mapping(address => AssetLiability)) beneficiaryAssetMap;
-    mapping(address => mapping(address => uint256)) payerCoinBalance;
-    mapping(address => uint256) beneficiaryCoinBalance;
+
+    // payer => beneficiaryHash => assetAddress => AssetLiability
+    mapping(address => mapping(string => mapping(address => AssetLiability))) payerAssetMap;
+    // beneficiaryHash => assetAddress => AssetLiability
+    mapping(string => mapping(address =>  AssetLiability)) beneficiaryAssetMap;
+    mapping(address => mapping(string => uint256)) payerCoinBalance;
+    mapping(string => uint256) beneficiaryCoinBalance;
 
     address public immutable IDRISS_ADDR;
     uint256 public immutable TRANSFER_EXPIRATION_IN_SECS;
@@ -53,24 +56,10 @@ contract sendToHash is Ownable {
     //      _;
     //  }
 
-    event AssetTransferred(
-        address indexed to,
-        address indexed from,
-        address indexed assetContractAddress,
-        uint256 amount
-    );
-    event AssetClaimed(
-        address indexed to,
-        address indexed from,
-        address indexed assetContractAddress,
-        uint256 amount
-    );
-    event AssetTransferReverted(
-        address indexed to,
-        address indexed from,
-        address indexed assetContractAddress,
-        uint256 amount
-    );
+    event AssetTransferred(address indexed to, address indexed from, address indexed assetContractAddress, uint256 amount);
+    event AssetClaimed(address indexed to, address indexed from, address indexed assetContractAddress, uint256 amount);
+    event AssetTransferReverted(address indexed to, address indexed from, address indexed assetContractAddress, uint256 amount);
+
 
     constructor(uint256 _transferExpirationInSecs, address _IDrissAddr) {
         TRANSFER_EXPIRATION_IN_SECS = _transferExpirationInSecs;
@@ -78,8 +67,8 @@ contract sendToHash is Ownable {
     }
 
     /**
-     * @notice This function allows a user to send tokens or coins to other IDriss. They are
-     *         being kept in an escrow until
+     * @notice This function allows a user to send tokens or coins to other IDriss. They are 
+     *         being kept in an escrow until it's claimed by beneficiary or reverted by payer
      * @dev Note that you have to approve this contract to handle ERCs on user's behalf
      */
     function sendToAnyone(
@@ -87,21 +76,19 @@ contract sendToHash is Ownable {
         uint256 _amount,
         AssetType _assetType,
         address _assetContractAddress,
-        uint256[] calldata _assetIds
+        uint256 [] calldata _assetIds
     ) external payable {
         //TODO: implement + reentrancy guard
 
-        uint256 calculatedClaimableUntil = block.timestamp +
-            TRANSFER_EXPIRATION_IN_SECS;
-        address ownerIDrissAddr = _getAddressFromHash(_IDrissHash);
+        uint256 calculatedClaimableUntil = block.timestamp + TRANSFER_EXPIRATION_IN_SECS;
+        console.log("asset type id: %s", uint(_assetType));
 
         if (_assetType == AssetType.Coin) {
-            beneficiaryCoinBalance[ownerIDrissAddr] += msg.value;
-            payerCoinBalance[msg.sender][ownerIDrissAddr] += msg.value;
+            console.log("asset type: coin");
+            beneficiaryCoinBalance[_IDrissHash] += msg.value;
+            payerCoinBalance[msg.sender][_IDrissHash] += msg.value;
         } else {
-            AssetLiability memory beneficiaryAsset = beneficiaryAssetMap[
-                ownerIDrissAddr
-            ][_assetContractAddress];
+            AssetLiability memory beneficiaryAsset = beneficiaryAssetMap[_IDrissHash][_assetContractAddress];
 
             if (beneficiaryAsset.claimableUntil == 0) {
                 beneficiaryAsset = AssetLiability({
@@ -109,46 +96,36 @@ contract sendToHash is Ownable {
                     claimableUntil: calculatedClaimableUntil,
                     assetIds: _assetIds
                 });
+
             } else {
                 beneficiaryAsset.amount += _amount;
                 beneficiaryAsset.claimableUntil = calculatedClaimableUntil;
+            }
 
-                if (_assetType == AssetType.Token) {
-                    _sendTokenAssetFrom(
-                        AssetLiability({
-                            amount: _amount,
-                            claimableUntil: calculatedClaimableUntil,
-                            assetIds: new uint256[](0)
-                        }),
-                        msg.sender,
-                        address(this),
-                        _assetContractAddress
-                    );
+                            if (_assetType == AssetType.Token) {
+                    console.log("asset type: token");
+                    _sendTokenAssetFrom(AssetLiability({
+                    amount: _amount,
+                    claimableUntil: calculatedClaimableUntil,
+                    assetIds: new uint256[](0)
+                }), msg.sender, address(this), _assetContractAddress);
                 } else if (_assetType == AssetType.NFT) {
+                    console.log("asset type: NFT");
                     for (uint256 i = 0; i < _assetIds.length; i++) {
                         //https://docs.soliditylang.org/en/v0.8.12/types.html#allocating-memory-arrays
                         //beneficiaryAsset.assetIds.push(_assetIds[i]);
                     }
 
-                    _sendNFTAsset(
-                        AssetLiability({
-                            amount: _amount,
-                            claimableUntil: calculatedClaimableUntil,
-                            assetIds: _assetIds
-                        }),
-                        msg.sender,
-                        address(this),
-                        _assetContractAddress
-                    );
+                _sendNFTAsset(AssetLiability({
+                    amount: _amount,
+                    claimableUntil: calculatedClaimableUntil,
+                    assetIds: _assetIds
+                }), msg.sender, address(this), _assetContractAddress);
+                
                 }
-            }
 
-            payerAssetMap[msg.sender][ownerIDrissAddr][
-                _assetContractAddress
-            ] = beneficiaryAsset; // TODO:change
-            beneficiaryAssetMap[ownerIDrissAddr][
-                _assetContractAddress
-            ] = beneficiaryAsset;
+            payerAssetMap[msg.sender][_IDrissHash][_assetContractAddress] = beneficiaryAsset; // TODO:change
+            beneficiaryAssetMap[_IDrissHash][_assetContractAddress] = beneficiaryAsset;
         }
     }
 
@@ -160,8 +137,10 @@ contract sendToHash is Ownable {
     function claim(
         string memory _IDrissHash,
         AssetType _assetType,
-        address _assetContractAddress
-    ) external payable {
+        address _assetContractAddress)
+        external
+        payable
+    {
         //TODO: implement
         address ownerIDrissAddr = _getAddressFromHash(_IDrissHash);
     }
@@ -171,121 +150,72 @@ contract sendToHash is Ownable {
         AssetType _assetType,
         address _assetContractAddress
     ) external view returns (uint256) {
-        address ownerIDrissAddr = _getAddressFromHash(_IDrissHash);
-
         if (_assetType == AssetType.Coin) {
-            return beneficiaryCoinBalance[ownerIDrissAddr];
-        } else if (
-            _assetType == AssetType.Token || _assetType == AssetType.NFT
-        ) {
-            return
-                beneficiaryAssetMap[ownerIDrissAddr][_assetContractAddress]
-                    .amount;
+            return beneficiaryCoinBalance[_IDrissHash];
+        } else if (_assetType == AssetType.Token || _assetType == AssetType.NFT) {
+            return beneficiaryAssetMap[_IDrissHash][_assetContractAddress].amount;
         }
-
+        
         return 0;
     }
 
     /**
      * @notice This function allows a user to revert sending tokens to other IDriss and claim them back
      */
-    //TODO: implement -> transfering tokens + checks + reentrancyGuard
+     //TODO: implement -> transfering tokens + checks + reentrancyGuard
     function revertPayment(
         string memory _IDrissHash,
         AssetType _assetType,
-        address _assetContractAddress
-    ) external {
+        address _assetContractAddress)
+        external {
         address ownerIDrissAddr = _getAddressFromHash(_IDrissHash);
-        uint256 amountToRevert = 0;
+        uint256 amountToRevert = 0; 
 
         if (_assetType == AssetType.Coin) {
-            amountToRevert = payerCoinBalance[msg.sender][ownerIDrissAddr];
-            beneficiaryCoinBalance[ownerIDrissAddr] -= amountToRevert;
-            payerCoinBalance[msg.sender][ownerIDrissAddr] = 0;
-            (bool sent, ) = address(this).call{value: amountToRevert}("");
+            amountToRevert = payerCoinBalance[msg.sender][_IDrissHash];
+            beneficiaryCoinBalance[_IDrissHash] -= amountToRevert;
+            payerCoinBalance[msg.sender][_IDrissHash] = 0;
+            (bool sent,) = address(this).call{value: amountToRevert}("");
             require(sent, "Failed to  withdraw");
         } else {
-            amountToRevert = payerAssetMap[msg.sender][ownerIDrissAddr][
-                _assetContractAddress
-            ].amount;
-            AssetLiability storage beneficiaryAsset = beneficiaryAssetMap[
-                ownerIDrissAddr
-            ][_assetContractAddress];
+            amountToRevert = payerAssetMap[msg.sender][_IDrissHash][_assetContractAddress].amount;
+            AssetLiability storage beneficiaryAsset = beneficiaryAssetMap[_IDrissHash][_assetContractAddress];
 
-            delete payerAssetMap[msg.sender][ownerIDrissAddr][
-                _assetContractAddress
-            ];
+            delete payerAssetMap[msg.sender][_IDrissHash][_assetContractAddress];
             beneficiaryAsset.amount -= uint128(amountToRevert);
 
             if (_assetType == AssetType.NFT) {
-                _sendNFTAsset(
-                    beneficiaryAsset,
-                    address(this),
-                    msg.sender,
-                    _assetContractAddress
-                );
+                _sendNFTAsset(beneficiaryAsset, address(this), msg.sender, _assetContractAddress);
             } else if (_assetType == AssetType.Token) {
-                _sendTokenAsset(
-                    beneficiaryAsset,
-                    msg.sender,
-                    _assetContractAddress
-                );
+                _sendTokenAsset(beneficiaryAsset, msg.sender, _assetContractAddress);
             }
         }
 
-        emit AssetTransferReverted(
-            ownerIDrissAddr,
-            msg.sender,
-            _assetContractAddress,
-            amountToRevert
-        );
+        emit AssetTransferReverted(ownerIDrissAddr, msg.sender, _assetContractAddress, amountToRevert);
     }
 
-    function _sendNFTAsset(
-        AssetLiability memory _asset,
-        address _from,
-        address _to,
-        address _contractAddress
-    ) internal {
+    function _sendNFTAsset(AssetLiability memory _asset, address _from, address _to, address _contractAddress) internal {
         IERC721 nft = IERC721(_contractAddress);
         for (uint256 i = 0; i < _asset.assetIds.length; i++) {
-            nft.safeTransferFrom(
-                address(this),
-                msg.sender,
-                _asset.assetIds[i],
-                ""
-            );
+            nft.safeTransferFrom (_from, _to, _asset.assetIds[i], "");
         }
     }
 
-    function _sendTokenAsset(
-        AssetLiability memory _asset,
-        address _to,
-        address _contractAddress
-    ) internal {
+    function _sendTokenAsset(AssetLiability memory _asset, address _to, address _contractAddress) internal {
         IERC20 token = IERC20(_contractAddress);
 
         bool sent = token.transfer(_to, _asset.amount);
         require(sent, "Failed to transfer token");
     }
 
-    function _sendTokenAssetFrom(
-        AssetLiability memory _asset,
-        address _from,
-        address _to,
-        address _contractAddress
-    ) internal {
+    function _sendTokenAssetFrom(AssetLiability memory _asset, address _from, address _to, address _contractAddress) internal {
         IERC20 token = IERC20(_contractAddress);
 
         bool sent = token.transferFrom(_from, _to, _asset.amount);
         require(sent, "Failed to transfer token");
     }
 
-    function _getAddressFromHash(string memory _IDrissHash)
-        internal
-        view
-        returns (address)
-    {
+    function _getAddressFromHash (string memory _IDrissHash) internal view returns (address) {
         //TODO: check if the address is valid. Revert otherwise
         return IDriss(IDRISS_ADDR).IDrissOwners(_IDrissHash);
     }
