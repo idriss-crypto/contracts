36e473a5607f231d699852c1fed9f031e6118317
Rafal Kalinowski <kalinowskirafal.fm@gmail.com>
2022-06-14 08:09:42 +0200

    adding mock token and nft

--- contracts/IDrissRegistryMock.sol
+++ contracts/IDrissRegistryMock.sol
@@ -1,6 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MockToken is ERC20{
+    constructor() ERC20("MockToken", "MCKT"){
+        _mint(msg.sender,1000*10**18);
+    }
+}
+
+contract MockNFT is ERC721{
+    constructor() ERC721("MockNFT", "MNFT"){
+        _mint(msg.sender,1000*10**18);
+    }
+}
+
 contract IDriss {
     mapping(string => address) public IDrissOwnersMap;
 
@@ -12,12 +27,15 @@ contract IDriss {
     function getIDriss(string memory _hash)
         external
         view
-        returns (string memory)
-    {
-        return "mock";
-    }
+        returns (string memory) {
+            return "mock";
+        }
 
-    function IDrissOwners(string memory _hash) external view returns (address) {
-        return IDrissOwnersMap[_hash];
-    }
+    function IDrissOwners(string memory _hash)
+        external
+        view
+        returns (address) {
+            return IDrissOwnersMap[_hash];
+        }
 }
+
