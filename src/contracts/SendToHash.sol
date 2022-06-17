d15d7fc34da203adc04f1ecef44483005c1be3d1
Rafal Kalinowski <kalinowskirafal.fm@gmail.com>
2022-06-17 22:15:59 +0200

    changing implementation to get address as string changed to address

--- src/contracts/SendToHash.sol
+++ src/contracts/SendToHash.sol
@@ -335,8 +335,45 @@ contract SendToHash is ISendToHash, Ownable, ReentrancyGuard, IERC721Receiver, I
         view
         returns (address IDrissAddress)
     {
-        IDrissAddress = IDriss(IDRISS_ADDR).IDrissOwners(_IDrissHash);
+        string memory IDrissString = IDriss(IDRISS_ADDR).getIDriss(_IDrissHash);
+        IDrissAddress = _safeHexStringToAddress(IDrissString);
         _checkNonZeroAddress(IDrissAddress, "Address for the IDriss hash cannot resolve to 0x0");
+    }
+
+    /**
+    * @notice Change a single character from ASCII to 0-F hex value; revert on unparseable value
+    */
+    function _fromHexChar(uint8 c) internal pure returns (uint8) {
+        if (bytes1(c) >= bytes1('0') && bytes1(c) <= bytes1('9')) {
+            return c - uint8(bytes1('0'));
+        } else if (bytes1(c) >= bytes1('a') && bytes1(c) <= bytes1('f')) {
+            return 10 + c - uint8(bytes1('a'));
+        } else if (bytes1(c) >= bytes1('A') && bytes1(c) <= bytes1('F')) {
+            return 10 + c - uint8(bytes1('A'));
+        } else {
+            revert("Unparseable hex character found in address.");
+        }
+    }
+
+    /**
+    * @notice Get address from string. Revert if address is invalid.
+    */    
+    function _safeHexStringToAddress(string calldata s) internal pure returns (address) {
+        bytes memory ss = bytes(s);
+        require(ss.length == 42, "Address length is invalid");
+        bytes memory _bytes = new bytes(ss.length / 2);
+        address resultAddress;
+
+        for (uint256 i = 0; i < ss.length / 2; i++) {
+            _bytes[i] = bytes1(_fromHexChar(uint8(ss[2*i])) * 16 +
+                        _fromHexChar(uint8(ss[2*i+1])));
+        }
+
+        assembly {
+            resultAddress := div(mload(add(add(_bytes, 0x20), 1)), 0x1000000000000000000000000)
+        }
+
+        return resultAddress;
     }
 
     function _getMaticUsdPrice() internal view returns (uint) {
