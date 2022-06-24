931a1986fae103d09012e3e618e5567801579c13
Rafal Kalinowski <kalinowskirafal.fm@gmail.com>
2022-06-24 11:36:21 +0200

    small config comments rearrange

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,8 +19,10 @@ const config: HardhatUserConfig = {
   typechain: {
     outDir: 'src/types',
     target: 'ethers-v5',
-    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
-    externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    alwaysGenerateOverloads: false,
+    // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    externalArtifacts: ['externalArtifacts/*.json'],
   },
   paths: {
     artifacts: 'src/artifacts',
