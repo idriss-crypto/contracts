e03b8068385d95ad4346e88ba9c9cba6eb031156
deliriusz <kalinowskirafal.fm@gmail.com>
2022-06-26 15:58:25 +0200

    adding info on array inside mapping

--- src/contracts/structs/IDrissStructs.sol
+++ src/contracts/structs/IDrissStructs.sol
@@ -3,5 +3,7 @@ pragma solidity ^0.8.7;
 
 struct AssetLiability {
     uint256 amount;
+    // IMPORTANT - when deleting AssetLiability, please remove assetIds array first. Reference:
+    // https://github.com/crytic/slither/wiki/Detector-Documentation#deletion-on-mapping-containing-a-structure
     mapping (address => uint256[]) assetIds;
 }
