e629b9ad5326406f9a6720ebfd9951341d8991cc
deliriusz <deliriusz.eth@gmail.com>
2022-10-18 14:55:14 +0200

    bumping Soliditry version to most recent one

--- src/contracts/mocks/SendToHashMock.sol
+++ src/contracts/mocks/SendToHashMock.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity 0.8.17;
 
 import { ISendToHash } from "../interfaces/ISendToHash.sol";
 import { SendToHash } from "../SendToHash.sol"; 
