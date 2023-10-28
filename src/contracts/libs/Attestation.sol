acc05bb04b586d48c802e0e45ec19bd7407ef8fe
lennardevertz <levertz@idriss.xyz>
2023-10-28 23:29:34 +0200

    - change solidity versions - use EAS_SCHEMA as constructor arg instead of hardcoded value

--- src/contracts/libs/Attestation.sol
+++ src/contracts/libs/Attestation.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.17;
+pragma solidity 0.8.19;
 
 import { IEAS, AttestationRequest, AttestationRequestData } from "../interfaces/IEAS.sol";
 import { NO_EXPIRATION_TIME, EMPTY_UID } from "./Common.sol";
@@ -12,18 +12,20 @@ contract PublicGoodAttester {
     error InvalidEAS();
 
     // The address of the global EAS contract.
-    IEAS private immutable _eas;
+    IEAS private immutable EAS;
+    bytes32 public immutable EAS_SCHEMA;
 
     /**
      * @dev Creates a new ExampleAttester instance.
      *
      * @param eas The address of the global EAS contract.
      */
-    constructor(address eas) {
+    constructor(address eas, bytes32 easSchema) {
         if (eas == address(0)) {
             revert InvalidEAS();
         }
-        _eas = IEAS(eas);
+        EAS = IEAS(eas);
+        EAS_SCHEMA = easSchema;
     }
 
     /**
@@ -33,9 +35,9 @@ contract PublicGoodAttester {
      */
     function _attestDonor(address _publicGood) internal returns (bytes32) {
         return
-            _eas.attest(
+            EAS.attest(
                 AttestationRequest({
-                    schema: 0x28b73429cc730191053ba7fe21e17253be25dbab480f0c3a369de5217657d925,
+                    schema: EAS_SCHEMA,
                     data: AttestationRequestData({
                         recipient: msg.sender, // the supporter receives the attestation
                         expirationTime: NO_EXPIRATION_TIME, // No expiration time
